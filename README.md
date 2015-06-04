c4d6f06f4a0a01445096e4b1a52cb3acc338fc51
mvillaronga-medkoder <mvillaronga@medkoder.com>
2015-06-04 02:30:11 -0500

    Created intial project and lots of setup with crazy javascript pakages and package managers.  default class defintions in the example provided does not work properly so needed to muck about with that.  Have the xample mostly working up to the initial switch to the navigation piece

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ Work with new aurelia framework to test its viability within the normal medkoder
 Based off of the documentation at http://aurelia.io/docs.html and http://aurelia.io/get-started.html.  
 
 It may advance to integrating with the medkoder architecture after the intial getting starteds are up and running.
+
+Dependancies
+NodeJS - https://nodejs.org/
+NodeJs - v1.1 tools for VS2015RC: https://nodejstools.codeplex.com/releases/view/614706
+
---
+++ aurelia-tryout-web/wwwroot/app.js
@@ -0,0 +1,22 @@
+﻿import 'bootstrap'
+
+export class Welcome {
+    //the below along with several other variations do not work.  settled on the changes you see below
+    //heading = 'Welcome to the Aurelia Navigation App!';
+    //firstName = 'John';
+    //lastName = 'Doe';
+
+    constructor() {
+        this.firstName = 'Buckaroo';
+        this.lastName = 'Bonsai';
+        this.heading = 'Welcome to my lame Aurelia Tutorial'
+    }
+
+    get fullName() {
+        return `${this.firstName} ${this.lastName}`;
+  }
+
+  welcome() {
+    alert(`Welcome, ${this.fullName}!`);
+  }
+}
